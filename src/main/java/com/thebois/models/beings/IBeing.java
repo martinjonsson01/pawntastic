e372fc910433b8f94004e106a62d65db866841da
Martin <martinjonsson01@gmail.com>
2021-09-15 19:30:50 +0200

    Add role getters and setters to IBeing

--- src/main/java/com/thebois/models/beings/IBeing.java
+++ src/main/java/com/thebois/models/beings/IBeing.java
@@ -1,6 +1,7 @@
 package com.thebois.models.beings;
 
 import com.thebois.models.Position;
+import com.thebois.models.beings.roles.Role;
 
 /**
  * An entity.
@@ -10,8 +11,22 @@ public interface IBeing {
     /**
      * Gets the current location.
      *
-     * @return The current location.
+     * @return The current location
      */
     Position getPosition();
+
+    /**
+     * Gets the role.
+     *
+     * @return The role
+     */
+    Role getRole();
+
+    /**
+     * Sets the role.
+     *
+     * @param role The role
+     */
+    void setRole(Role role);
 
 }
