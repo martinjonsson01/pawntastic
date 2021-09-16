0a7efd5745ae889fbc8fdbb384b6c9d14dc6e228
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2021-09-16 09:55:09 +0200

    Add first red test for world

---
+++ src/test/java/com/thebois/WorldTests.java
@@ -0,0 +1,20 @@
+package com.thebois;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.thebois.models.World;
+
+public class WorldTests {
+
+    @Test
+    public void worldInitiated() {
+
+        final int[][] testWorld = new int[2][2];
+
+        final World world = new World();
+        final int[][] worldMatrix = world.getWorld();
+        assertThat(testWorld.length).isEqualTo(worldMatrix.length);
+    }
+}
