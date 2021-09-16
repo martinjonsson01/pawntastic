bf91a0ba1ecd5ab99bdad87b17efb49077139bfb
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2021-09-16 16:08:02 +0200

    Improve worldInitiated test

--- src/test/java/com/thebois/models/WorldTests.java
+++ src/test/java/com/thebois/models/WorldTests.java
@@ -10,10 +10,29 @@ public class WorldTests {
 
     @Test
     public void worldInitiated() {
-        final int[][] testWorld = new int[2][2];
-
+        // Arrange
+        final ITile[][] testWorld = new ITile[2][2];
         final World world = new World(2);
+
+        // Act
         final ITile[][] worldMatrix = world.getWorld();
-        assertThat(testWorld.length).isEqualTo(worldMatrix.length);
+
+        // Assert
+        assertThat(matrixEquals(worldMatrix, testWorld)).isTrue();
+    }
+
+    private boolean matrixEquals(ITile[][] worldMatrix1, ITile[][]worldMatrix2) {
+        for (int y = 0; y < worldMatrix2.length; y++) {
+            final ITile[] row = worldMatrix2[y];
+            for (int x = 0; x < row.length; x++) {
+                if (worldMatrix1[y][x] != null) {
+                    if (!worldMatrix1[y][x].equals(worldMatrix2[y][x])) return false;
+                }
+                else {
+                    if (worldMatrix2[y][x] != null) return false;
+                }
+            }
+        }
+        return true;
     }
 }
