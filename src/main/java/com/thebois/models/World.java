11177af1df3b217b43311a4e42012730e218012e
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2021-09-16 09:43:15 +0200

    Add World class

---
+++ src/main/java/com/thebois/models/World.java
@@ -0,0 +1,17 @@
+package com.thebois.models;
+
+/**
+ * World creates a matrix and keeps track of all the structures and resources in the game world.
+ */
+public class World implements IFinder {
+
+    /**
+     * Locates an object in the world and returns it.
+     *
+     * @return Object.
+     */
+    public Object find() {
+        return null;
+    }
+
+}
