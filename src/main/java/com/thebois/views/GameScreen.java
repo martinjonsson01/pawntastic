821eacafc7fc4162a78a6219008dee5dc102086b
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2021-09-16 13:51:25 +0200

    Add method in GameScren to render empty world

--- src/main/java/com/thebois/views/GameScreen.java
+++ src/main/java/com/thebois/views/GameScreen.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
+import com.thebois.models.World;
+import com.thebois.models.tiles.ITile;
+
 /**
  * The main screen displaying the world and UI.
  */
@@ -15,13 +18,16 @@ public class GameScreen implements Screen {
     private final ShapeRenderer batch;
     private final OrthographicCamera camera;
     private final FitViewport viewport;
-    private final float worldWidth = 800f;
-    private final float worldHeight = 480f;
-    private final float rectangleSize = 64f;
+    private final float worldWidth = 1000f;
+    private final float worldHeight = 1000f;
+    private final float rectangleSize = 20f;
     private final Color backgroundColor = new Color(0, 0, 0.2f, 1);
     private final int moveSpeed = 200;
-    private int posY = 0;
+    private int moveY = 0;
     private final Color blueColor = new Color(0, 0, 1, 1);
+    private final Color grassColor = new Color(0.005f, 0.196f, 0.107f, 1);
+    private final int worldSize = 50;
+    private final ITile[][] worldGrid;
 
     /**
      * Creates an instance of GameScreen.
@@ -30,6 +36,8 @@ public class GameScreen implements Screen {
      */
     public GameScreen(final ShapeRenderer batch) {
         this.batch = batch;
+
+        worldGrid = new World(worldSize).getWorld();
 
         // create the camera and the SpriteBatch
         camera = new OrthographicCamera();
@@ -47,15 +55,29 @@ public class GameScreen implements Screen {
         camera.update();
         batch.setProjectionMatrix(camera.combined);
 
+        renderWorld();
+
         batch.begin(ShapeRenderer.ShapeType.Filled);
         batch.setColor(blueColor);
         batch.rect(worldWidth / 2 - rectangleSize, worldHeight / 2 - rectangleSize, rectangleSize,
                    rectangleSize);
         batch.setColor(1, 1, 1, 1);
-        batch.rect(0, posY, rectangleSize, rectangleSize);
+        batch.rect(0, moveY, rectangleSize, rectangleSize);
         batch.end();
 
-        posY += moveSpeed * delta;
+        moveY += moveSpeed * delta;
+    }
+
+    private void renderWorld() {
+        batch.begin(ShapeRenderer.ShapeType.Filled);
+        batch.setColor(grassColor);
+        for (int posX = 0; posX < worldGrid.length; posX++) {
+            for (int posY = 0; posY < worldGrid[0].length; posY++) {
+                batch.rect(posX * rectangleSize, posY * rectangleSize, rectangleSize,
+                           rectangleSize);
+            }
+        }
+        batch.end();
     }
 
     @Override
