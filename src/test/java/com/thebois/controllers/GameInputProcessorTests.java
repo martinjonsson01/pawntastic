a4bc5b72f1b755fab902b28d49e8b97307dbe3c0
Mathias <mathias.pretot@gmail.com>
2021-09-16 16:29:28 +0200

    test the GameInputProcessor

--- src/test/java/com/thebois/controllers/GameInputProcessorTests.java
+++ src/test/java/com/thebois/controllers/GameInputProcessorTests.java
@@ -7,10 +7,16 @@ import static org.assertj.core.api.Assertions.*;
 public class GameInputProcessorTests {
 
     @Test
-    public void leftClickTest() {
+    public void leftClickIsPressedTest() {
         final GameInputProcessor gip = new GameInputProcessor();
         final Boolean isLeftClickPressed = gip.touchDown(0, 0, 0, 0);
         assertThat(isLeftClickPressed).as("Left click is pressed").isEqualTo(true);
     }
 
+    @Test
+    public void leftClickIsNotPressedTest() {
+        final GameInputProcessor gip = new GameInputProcessor();
+        final Boolean isLeftClickPressed = gip.touchDown(0, 0, 0, 1);
+        assertThat(isLeftClickPressed).as("Left click is not pressed").isEqualTo(false);
+    }
 }
