bbb3baa1874892aef3acb8d00ea0483edca38e10
Jacob Bredin <jacob@bredin.se>
2021-09-17 10:30:59 +0200

    Fix CheckStyle import error

--- src/test/java/com/thebois/models/beings/ColonyTests.java
+++ src/test/java/com/thebois/models/beings/ColonyTests.java
@@ -1,10 +1,10 @@
 package com.thebois.models.beings;
 
+import org.junit.jupiter.api.Test;
+
 import com.thebois.models.Position;
 
 import static org.assertj.core.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
 
 public class ColonyTests {
 
