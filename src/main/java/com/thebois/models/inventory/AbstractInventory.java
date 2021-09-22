cfd1cc81ca92f1cd8978e44cbe1113771fb5b871
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2021-09-22 16:17:18 +0200

    Add AbstractInventory class

---
+++ src/main/java/com/thebois/models/inventory/AbstractInventory.java
@@ -0,0 +1,9 @@
+package com.thebois.models.inventory;
+
+import java.util.ArrayList;
+
+public abstract class AbstractInventory {
+
+    private final ArrayList<IItem> itemHolder = new ArrayList<>();
+
+}
