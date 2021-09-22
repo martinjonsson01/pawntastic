cb42192cf066d1e96c78761362c0d489337e88a9
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2021-09-22 16:52:52 +0200

    Refactor and rename remove method for AbstractInvnetory

--- src/main/java/com/thebois/models/inventory/AbstractInventory.java
+++ src/main/java/com/thebois/models/inventory/AbstractInventory.java
@@ -6,12 +6,50 @@ public abstract class AbstractInventory {
 
     private final ArrayList<IItem> itemHolder = new ArrayList<>();
 
+    /**
+     * Items can be put into the inventory.
+     *
+     * @param item The item to be added to the inventory
+     */
     public void addItem(final IItem item) {
         itemHolder.add(item);
     }
 
-    public void removeItem(final IItem item) {
-        itemHolder.remove(item);
+    /**
+     * Removes and returns the first item of the specified type.
+     *
+     * @param itemType The item type to be taken
+     *
+     * @return The item removed from the inventory
+     */
+    public IItem takeItem(final ItemType itemType) {
+        if (doesItemTypeExist(itemType)) {
+            final int firstIndexOfItemType = getFirstIndexOf(itemType);
+            itemHolder.remove(firstIndexOfItemType);
+        }
+        else {
+            // No matching item.
+            return null;
+        }
+    }
+
+    private int getFirstIndexOf(final ItemType itemType) {
+        int index = -1;
+        for (final IItem item : itemHolder) {
+            if (item.getType().equals(itemType)) {
+                index = itemHolder.indexOf(item);
+            }
+        }
+        return index;
+    }
+
+    public boolean doesItemTypeExist(final ItemType itemType) {
+        for (final IItem item : itemHolder) {
+            if (item.getType().equals(itemType)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
