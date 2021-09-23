b753ebab0f9f7e96d77c496579a866d4367fa592
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2021-09-23 18:17:20 +0200

    Add tests for inventory in Colony

--- src/test/java/com/thebois/models/beings/ColonyTests.java
+++ src/test/java/com/thebois/models/beings/ColonyTests.java
@@ -3,16 +3,29 @@ package com.thebois.models.beings;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
 import com.thebois.models.Position;
+import com.thebois.models.inventory.AbstractInventory;
+import com.thebois.models.inventory.ColonyInventory;
+import com.thebois.models.inventory.items.IItem;
+import com.thebois.models.inventory.items.ItemType;
+import com.thebois.models.inventory.items.Log;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class ColonyTests {
+
+    public static Stream<Arguments> getItemTypes() {
+        return Stream.of(Arguments.of(ItemType.LOG), Arguments.of(ItemType.ROCK));
+    }
 
     @Test
     public void constructWithPositionsCreatesOneBeingPerPosition() {
@@ -47,6 +60,42 @@ public class ColonyTests {
         for (final IBeing pawn : pawns) {
             verify(pawn, times(1)).update();
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("getItemTypes")
+    public void emptyColonyInventoryIsEmpty(final ItemType itemType) {
+        // Arrange
+        final int beingCount = 1;
+        final Collection<IBeing> pawns = new ArrayList<>(beingCount);
+        for (int i = 0; i < beingCount; i++) {
+            pawns.add(Mockito.mock(IBeing.class));
+        }
+        final Colony colony = new Colony(pawns);
+
+        // Act
+        final IItem result = colony.takeItemFromColonyInventory(itemType);
+
+        // Assert
+        assertThat(result).isEqualTo(null);
+    }
+
+    @Test
+    public void canAddAndTakeItemToInventory() {
+        // Arrange
+        final int beingCount = 1;
+        final Collection<IBeing> pawns = new ArrayList<>(beingCount);
+        for (int i = 0; i < beingCount; i++) {
+            pawns.add(Mockito.mock(IBeing.class));
+        }
+        final Colony colony = new Colony(pawns);
+
+        // Act
+        colony.addItemToColonyInventory(new Log());
+        final IItem item = colony.takeItemFromColonyInventory(ItemType.LOG);
+
+        // Assert
+        assertThat(item.getType()).isEqualTo(ItemType.LOG);
     }
 
 }
