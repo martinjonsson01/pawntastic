1c9973ac8bd6dd4f50f4ac32e22ac38145584523
Jacob Bredin <jacob@bredin.se>
2021-10-13 21:39:17 +0200

    Remove equals and hash methods from Structure
    Since AbstractStructure is a domain entity, it should not be equal to any instance except itself, and therefor only needs the standard equals and hash methods inherited from Object.

--- src/main/java/com/thebois/models/world/structures/AbstractStructure.java
+++ src/main/java/com/thebois/models/world/structures/AbstractStructure.java
@@ -57,23 +57,6 @@ abstract class AbstractStructure implements IStructure {
     }
 
     @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        final AbstractStructure that = (AbstractStructure) o;
-        return position.equals(that.position) && structureType == that.structureType;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(position, structureType);
-    }
-
-    @Override
     public StructureType getType() {
         return structureType;
     }
