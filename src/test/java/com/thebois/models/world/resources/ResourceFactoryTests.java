054bced57bc07549e4a883504320cf8afdb057eb
Mathias <mathias.pretot@gmail.com>
2021-10-13 18:17:17 +0200

    Add tests for Resource Factory

---
+++ src/test/java/com/thebois/models/world/resources/ResourceFactoryTests.java
@@ -0,0 +1,39 @@
+package com.thebois.models.world.resources;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ResourceFactoryTests {
+
+    @Test
+    public void createResourceWithTreeEnumReturnsTreeResource() {
+        // Arrange
+        final int x = 0;
+        final int y = 0;
+        final IResource expectedTree = new Tree(x, y);
+        final IResource actualTree;
+
+        // Act
+        actualTree = ResourceFactory.createResource(ResourceType.TREE, x, y);
+
+        // Assert
+        assertThat(actualTree).isEqualTo(expectedTree);
+    }
+
+    @Test
+    public void createResourceWithWaterEnumReturnsWaterResource() {
+        // Arrange
+        final int x = 0;
+        final int y = 0;
+        final IResource expectedWater = new Water(x, y);
+        final IResource actualWater;
+
+        // Act
+        actualWater = ResourceFactory.createResource(ResourceType.WATER, x, y);
+
+        // Assert
+        assertThat(actualWater).isEqualTo(expectedWater);
+    }
+
+}
