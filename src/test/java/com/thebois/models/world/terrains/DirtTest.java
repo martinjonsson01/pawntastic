e3860af6b3a0eb4c7d5b5605de98d05a0659ee35
Mathias <mathias.pretot@gmail.com>
2021-10-13 18:27:32 +0200

    Add missing Dirt tests

--- src/test/java/com/thebois/models/world/terrains/DirtTest.java
+++ src/test/java/com/thebois/models/world/terrains/DirtTest.java
@@ -2,6 +2,8 @@ package com.thebois.models.world.terrains;
 
 import org.junit.jupiter.api.Test;
 
+import com.thebois.models.Position;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class DirtTest {
@@ -16,6 +18,34 @@ public class DirtTest {
 
         // Assert
         assertThat(type).isEqualTo(TerrainType.DIRT);
+    }
+
+    @Test
+    public void getCostReturnsZero() {
+        // Arrange
+        final Dirt dirt = new Dirt(0, 0);
+        final float expectedCost = 0;
+        final float actualCost;
+
+        // Act
+        actualCost = dirt.getCost();
+
+        // Assert
+        assertThat(actualCost).isEqualTo(expectedCost);
+    }
+
+    @Test
+    public void deepCloneableReturnsCopyThatIsEqualToOriginal() {
+        // Arrange
+        final Position position = new Position(0, 0);
+        final Dirt dirt = new Dirt(position);
+        final Dirt deepClone;
+
+        // Act
+        deepClone = dirt.deepClone();
+
+        // Assert
+        assertThat(deepClone).isEqualTo(dirt);
     }
 
 }
