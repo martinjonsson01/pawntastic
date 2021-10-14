a07ef67ba0c92500b27bd8cea666ac8a1b4fde18
Mathias <mathias.pretot@gmail.com>
2021-10-14 10:47:55 +0200

    Add missing javadoc for Terrain and Resource enum

--- src/main/java/com/thebois/models/world/resources/ResourceType.java
+++ src/main/java/com/thebois/models/world/resources/ResourceType.java
@@ -24,10 +24,36 @@ public enum ResourceType {
         this.seedPermutation = seedPermutation;
     }
 
+    /**
+     * Threshold value used for resource generation.
+     * <p>
+     * Used together with some kind of value generator and if the threshold is over, under or equal
+     * to the generated value then the resource should be created.
+     * </p>
+     *
+     * <p>
+     * Should be used like this: Value >= Threshold or Value <= Threshold.
+     * </p>
+     *
+     * @return The Threshold..
+     */
     public float getThreshold() {
         return threshold;
     }
 
+    /**
+     * Seed permutation used for resource generation.
+     *
+     * <p>
+     * Used to offset the resource from other resources.
+     * </p>
+     * <p>
+     * Should be used like this: BaseSeed "Some Operation" seedPermutation. Make sure to use the
+     * same operation for all terrain in order to not get unwanted results.
+     * </p>
+     *
+     * @return The seed permutation.
+     */
     public int getSeedPermutation() {
         return seedPermutation;
     }
--- src/main/java/com/thebois/models/world/terrains/TerrainType.java
+++ src/main/java/com/thebois/models/world/terrains/TerrainType.java
@@ -24,10 +24,36 @@ public enum TerrainType {
         this.seedPermutation = seedPermutation;
     }
 
+    /**
+     * Threshold value used for terrain generation.
+     * <p>
+     * Used together with some kind of value generator and if the threshold is over, under or equal
+     * to the generated value then the terrain should be created.
+     * </p>
+     *
+     * <p>
+     * Should be used like this: Value >= Threshold or Value <= Threshold.
+     * </p>
+     *
+     * @return The Threshold..
+     */
     public float getThreshold() {
         return threshold;
     }
 
+    /**
+     * Seed permutation used for terrain generation.
+     *
+     * <p>
+     * Used to offset the terrain from other terrain.
+     * </p>
+     * <p>
+     * Should be used like this: BaseSeed "Some Operation" seedPermutation. Make sure to use the
+     * same operation for all terrain in order to not get unwanted results.
+     * </p>
+     *
+     * @return The seed permutation.
+     */
     public int getSeedPermutation() {
         return seedPermutation;
     }
