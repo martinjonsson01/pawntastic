900b261414eb7c43184330d603dd626800bb4826
Mathias <mathias.pretot@gmail.com>
2021-10-20 20:39:30 +0200

    Add IPositionable

---
+++ src/main/java/com/thebois/models/IPositionable.java
@@ -0,0 +1,15 @@
+package com.thebois.models;
+
+/**
+ * Something that has a position.
+ */
+public interface IPositionable {
+
+    /**
+     * Gets the current location.
+     *
+     * @return The current location.
+     */
+    Position getPosition();
+
+}
