642bb1917419a99c05b2889ddb478875afef214e
Mathias <mathias.pretot@gmail.com>
2021-10-20 20:47:21 +0200

    Add IGiver

---
+++ src/main/java/com/thebois/models/beings/IGiver.java
@@ -0,0 +1,30 @@
+package com.thebois.models.beings;
+
+import com.thebois.models.IPositionable;
+import com.thebois.models.inventory.items.IItem;
+import com.thebois.models.inventory.items.ItemType;
+
+/**
+ * Something that can give out items.
+ */
+public interface IGiver extends IPositionable {
+
+    /**
+     * Take item of given type.
+     *
+     * @param itemType The type of item to take.
+     *
+     * @return The item.
+     */
+    IItem takeItem(ItemType itemType);
+
+    /**
+     * If the giver has the item.
+     *
+     * @param itemType The type of item.
+     *
+     * @return Whether the giver has the item or not.
+     */
+    boolean hasItem(ItemType itemType);
+
+}
